00964b8576e83a21d016793e315c4f3ce6bd08ec
andretheronsa <andretheronsa@gmail.com>
2019-12-03 14:21:58 +0200

    fix

--- app.py
+++ app.py
@@ -5,17 +5,6 @@ from flask_sqlalchemy import SQLAlchemy
 # Init app
 app = Flask(__name__)
 
-# Init DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
-db = SQLAlchemy(app)
-db.Model.metadata.reflect(db.engine)
-
-# Create DB schema
-class School(db.Model):
-    __tablename__ = 'schools-geocoded'
-    __table_args__ = { 'extend_existing': True }
-    LOC_CODE = db.Column(db.Text, primary_key=True)
-
 # Set routings
 @app.route("/")
 def get_location():
