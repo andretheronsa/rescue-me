570f54425457e6805525fd9b6556a6400ff2496c
andretheronsa <andretheronsa@gmail.com>
2019-12-06 11:45:03 +0200

    update

--- app.py
+++ app.py
@@ -4,8 +4,25 @@ from flask_sqlalchemy import SQLAlchemy
 # Init app
 app = Flask(__name__)
 
-# Set routings
-@app.route("/", methods=['GET', 'POST'])
+# Login screen 
+@app.route("/")
+def index():
+    pass
+    return render_template("")
+
+# Login screen 
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+# Generate a link for user
+@app.route("/generate-link")
+def generate_link():
+    pass
+    return render_template("")
+
+# Main tracking routing
+@app.route("/tracking", methods=['GET', 'POST'])
 def get_location():
     if request.method == 'POST':
         lat = request.args.get("latitude")
@@ -13,6 +30,12 @@ def get_location():
     else:
         lat = lon = 0
     return render_template("get-location.html", lat = lat, lon = lon)
+
+# Monitor patients database and map
+@app.route("/dashboard")
+def dashboard():
+    pass
+    return render_template("")
 
 # Run server
 if __name__ == '__main__':
