adcc6c4e45970e63f216ab1914407ebc6fb7e699
andretheronsa <andretheronsa@gmail.com>
2019-12-19 15:34:23 +0200

    add outdoor map

--- application/static/js/map.js
+++ application/static/js/map.js
@@ -2,7 +2,7 @@ var mymap = L.map('mapid').setView([latitude, longitude], 13);
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: 'mapbox/satellite-v9',
+    id: 'mapbox/outdoors-v11',
     accessToken: MAPBOX_API
 }).addTo(mymap);
 if(positionAccuracy != 'None'){
