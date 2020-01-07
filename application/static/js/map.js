3bdc20c634a3d07b304ce5345719dcb82804b02d
andretheronsa <andretheronsa@gmail.com>
2020-01-07 14:31:05 +0200

    colour and zoom

--- application/static/js/map.js
+++ application/static/js/map.js
@@ -1,4 +1,4 @@
-var map = L.map('mapid').setView([latitude, longitude], 16);
+var map = L.map('mapid').setView([latitude, longitude], 14);
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 22,
@@ -30,6 +30,6 @@ if(point == 'true'){
         ).openPopup()
    };
 if(line == 'true'){
-    var polyline = L.polyline(line_data, {color: 'red'}).addTo(map);
+    var polyline = L.polyline(line_data, {color: 'blue'}).addTo(map);
     map.fitBounds(polyline.getBounds());
 };
