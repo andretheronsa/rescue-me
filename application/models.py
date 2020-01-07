2ecc83fbf7a0c4f1a7e7a8b12883ef87b07e01f4
andretheronsa <andretheronsa@gmail.com>
2020-01-07 11:00:52 +0200

    add timezone

--- application/models.py
+++ application/models.py
@@ -23,7 +23,7 @@ class User(UserMixin, db.Model):
     tracks = db.relationship('Track', backref='username', lazy='dynamic')
     
   
-    # Functions
+    # Functions #
     def set_password(self, password):
         self.password_hash = generate_password_hash(password, method='sha256')
 
